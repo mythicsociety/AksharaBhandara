b5a5978266d582b7f7e5829d17ddf4ef210d20bc
mythicsociety <3dscanning.mythicsociety@gmail.com>
2023-06-06 22:31:54 +0530

    find shasana by id

--- src/components/Shasanagalu.vue
+++ src/components/Shasanagalu.vue
@@ -9,7 +9,7 @@ import axios from 'axios';
 <script>
 
 export default {
-    props: {},
+    props: { id: String },
     data() {
         return {
             selectedShasana: null,
@@ -17,12 +17,14 @@ export default {
             publicPath: import.meta.env.BASE_URL,
             groupedShasanaDetails: [],
             totalLines: 0,
-            totalCharacters: 0
+            totalCharacters: 0,
+            shasanaImagePath: "",
+            shasanaId: 0
         }
     },
     watch: {
         selectedShasana(newValue, oldValue) {
-
+            let selectedShasanaDetails = this.shasanas.find(s => s.key === newValue);
             axios.get(`${this.publicPath}./assets/json/shasanas/${newValue}.json`)
                 .then(response => {
                     //group letters by lineNumber
@@ -37,6 +39,9 @@ export default {
                     this.totalLines = sortedMap.size;
                     this.totalCharacters = Array.from(sortedMap.values()).flat().length;
 
+                    this.shasanaImagePath = selectedShasanaDetails.imagePath;
+                    this.shasanaId = selectedShasanaDetails.id;
+
                 });
         }
     },
@@ -45,6 +50,18 @@ export default {
             axios.get(`${this.publicPath}./assets/json/shasanas.json`)
                 .then(response => {
                     this.shasanas = response.data.sort((a, b) => parseInt(a.year) - parseInt(b.year));
+
+                    let shasanaId = this.$route.query.id;
+
+                    if (shasanaId !== undefined) {
+                        let selectedShasana = this.shasanas.find(s => s.id === parseInt(shasanaId));
+                        if (selectedShasana == undefined) {
+                            alert("No inscription found for ID " + shasanaId)
+                        } else {
+                            this.selectedShasana = selectedShasana.key;
+                            this.shasanaId = shasanaId;
+                        }
+                    }
                 })
                 .catch(error => {
                     console.log(error);
@@ -63,22 +80,28 @@ export default {
         <h2>ಶಾಸನಗಳು</h2>
 
         <div style="padding: 10px;">
-            <label for="cars" style="color: black;">ಒಂದು ಆಯ್ಕೆಮಾಡಿ:</label>
+            <label for="shasana" style="color: black;">ಒಂದು ಆಯ್ಕೆಮಾಡಿ:</label>
 
-            <select id="cars" v-model="selectedShasana" style="margin: 10px;">
+            <select id="shasana" v-model="selectedShasana" style="margin: 10px;">
                 <option v-for="sha in shasanas" :key="sha.id" :value="sha.key">{{ sha.displayName }}</option>
             </select>
 
-            <p v-if="selectedShasana !== null">ಆಯ್ಕೆ ಮಾಡಿದ ಶಾಸನದಲ್ಲಿ {{ totalLines }} ಸಾಲುಗಳು ಮತ್ತು ಒಟ್ಟು {{ totalCharacters }} ಅಕ್ಷರಗಳಿವೆ</p>
+            <p v-if="selectedShasana !== null">ಆಯ್ಕೆ ಮಾಡಿದ ಶಾಸನದಲ್ಲಿ (ID: {{ shasanaId }}) {{ totalLines }} ಸಾಲುಗಳು ಮತ್ತು ಒಟ್ಟು {{ totalCharacters
+            }} ಅಕ್ಷರಗಳಿವೆ</p>
         </div>
 
-        <div v-for="(group, index) in groupedShasanaDetails" :class="flex-container-parent">
-            <p>ಕೆಳಗಿನ ಸಾಲಿನಲ್ಲಿ {{ group[1].length }} ಅಕ್ಷರಗಳಿವೆ - ಸಾಲು {{ index+1 }}</p>
+        <div v-for="(group, index) in groupedShasanaDetails" :class="flex - container - parent">
+            <p>ಕೆಳಗಿನ ಸಾಲಿನಲ್ಲಿ {{ group[1].length }} ಅಕ್ಷರಗಳಿವೆ - ಸಾಲು {{ index + 1 }}</p>
             <div class="flex-container-no-gap">
                 <BasicLetter v-for="letter in group[1]" :image_src="letter.filePath" :showLetterText="false"
-                    :imageSizePx="50" :showLetterYear="false" :displayText="letter.kannadaWord"/>
+                    :imageSizePx="50" :showLetterYear="false" :displayText="letter.kannadaWord" />
             </div>
 
+        </div>
+
+        <div style="display: inline-block; padding-top: 25px">
+            <img v-if="shasanaImagePath !== ''" :src="`${publicPath}./assets/${shasanaImagePath}`" :alt="`Shasana image`"
+                style="display: block;" />
         </div>
 
     </div>

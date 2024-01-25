fb7c2f7754d7bbd25623e0ac7f69c04b0246a0be
mythicsociety <3dscanning.mythicsociety@gmail.com>
2024-01-25 21:27:31 +0530

    gunitakshara json update

--- src/components/SubComponents/IAST.vue
+++ src/components/SubComponents/IAST.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-        <label>
-            <input type="checkbox" v-model="showIAST" @change="updateValue" />
-            {{ showIAST ? 'Show in Kannada' : 'Show IAST form' }}
+    <div class="bottom-border">
+        <label class="switch">
+            <input type="checkbox" v-model="showIAST" @change="updateValue">
+            <span class="slider"></span>
         </label>
+        <p>{{ showIAST ? $t('letter.showKannada') : $t('letter.showIAST') }}</p>
     </div>
 </template>
 
@@ -22,3 +23,65 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.switch {
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    width: 60px;
+    height: 34px;
+}
+
+.switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+.slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+}
+
+input:checked+.slider {
+    background-color: #2196F3;
+}
+
+input:focus+.slider {
+    box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked+.slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+}
+
+.slider.round {
+    border-radius: 34px;
+}
+
+.slider.round:before {
+    border-radius: 50%;
+}
+</style>
